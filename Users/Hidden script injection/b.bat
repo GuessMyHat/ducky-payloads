50f3e4f7b90775395d62950046c392864d18400a
GuessMyHat <98121795+GuessMyHat@users.noreply.github.com>
2022-07-21 23:23:13 +0200

    Update b.bat

--- Users/Hidden script injection/b.bat
+++ Users/Hidden script injection/b.bat
@@ -1,1 +1,5 @@
-REM Write your code here.
+REM This is a comment.
+REM Remember to change the directory of your cmd shell when you run it with "cd /D [PATH]" switching to the root of your rubber ducky containing all the scripts
+REM cd /D will allow you to change also the drive letter (i.e. cd /D E:\root)
+
+REM Now you can write your own code, have fun!
